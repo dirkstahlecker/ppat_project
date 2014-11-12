0849bf7663e6d4dd2353f0fd9bfabfc730be9ec5
Arthi Vezhavendan <arthivez@mit.edu>
2014-11-12 06:39:02 -0500

    buttons on floor plan do not work yet
    but plans are still displayed
    TODO: FIX.

---
+++ userinterface/js/scripts.js
@@ -0,0 +1,7 @@
+function showFloor(updateFloor){
+  //$('floorplan').class('hidden')
+  //$('floorsummary').class('hidden')
+  document.getElementById("fultonSouthFloor4").src="../userinterface/img/TestImagePhoebe/caution.png"
+    
+}
+
