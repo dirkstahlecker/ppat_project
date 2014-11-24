065286b137b356a8ab12b5063a8b684d0609323b
Dirk <stahdirk@mit.edu>
2014-11-23 22:59:48 -0500

    legend added

--- userinterface/js/maps.js
+++ userinterface/js/maps.js
@@ -1,54 +1,3 @@
-
-
-//************ ARTHI'S FUNCTIONS ************//
-/*
-function showFloor(updateFloor){
-  newImageSource = "../userinterface/img/fultonhall/" + updateFloor +".jpg";
-  document.getElementById("fultonFloorImage").src = newImageSource;
-
-  //hide all text
-  hideAllText();
-
-  //show the relevant text
-  newTextID = "#fultonFloorText_" + updateFloor;
-  $(newTextID).removeClass('hidden');
-  $(newTextID).addClass('show');
-}
-
-function hideAllText(){
-  textIDs = ['floorSummaries', 'floor1', 'floor2', 'floor3', 'floor4']
-
-  for (i=0; i<textIDs.length; i++) {
-    newID = "#fultonFloorText_" + textIDs[i];
-    $(newID).removeClass('show');
-    $(newID).addClass('hidden');
-  }
-}
-
-// $(".nav-tabs").find("a").click(function(){
-//     $(".nav-tabs").find("a").removeClass("active");
-//     $(this).addClass("active");
-// });
-
-
-//opens modal with Fulton Floor plans and details:
-function showFultonModal(event){
-  $('#myFultonModal').modal('toggle')
-}
-
-
-//opens modal with Fulton Floor plans and details:
-function showStokesModal(event){
-  $('#myFultonModal').modal('toggle')
-}*/
-
-
-//***************************************************************************************************//
-//*************************************** DIRK ******************************************************//
-//***************************************************************************************************//
-
-
-
 function showFloor(buildingID, updateFloor, numFloors){
 	console.log('in showFloor');
 	//newImageSource = "../userinterface/img/fultonhall/" + updateFloor +".jpg";
@@ -170,107 +119,6 @@ function addAlerts(map) {
 	});
 }
 
-/*
-	//doorLocations = [new google.maps.LatLng(42.334294,-71.169987), new google.maps.LatLng(42.334635,-71.169783)];
-
-	//instantiating door symbol
-	var wheelchairDoor = {
-		url: 'img/testImagePhoebe/wheelchair.jpg',
-		scaledSize: new google.maps.Size(25,25)
-	};
-	//instantiating pothole caution symbol
-	var potholeCaution = {
-		url: 'img/testImagePhoebe/caution.png',
-		scaledSize:new google.maps.Size(30,25)
-	}
-
-	//adding markers
-	markerArray = [];
-	function addMarkers(array, iconType){
-		for (i = 0; i < array.length; i++){
-			var marker = new google.maps.Marker({
-				position: array[i],
-				icon: iconType,
-				map: map,
-				draggable: false
-			});
-			markerArray.push(marker);
-		}
-	}
-
-	//index 0
-	addMarkers(potholeLocations, potholeCaution);
-	//indices 1-2
-	addMarkers(doorLocations, wheelchairDoor);
-
-	console.log('marker array: ');
-	console.log(markerArray);
-
-	//TODO: make this not ugly
-	//html content for each  marker popup
-	content = ['<div id="content"> <div id="siteNotice"></div>'+
-		'<h1 id="firstHeading" class="firstHeading">Pothole</h1>'+
-		'<div id="bodyContent">'+
-		'<p><b><img src = "../testImagePhoebe/pothole.jpg" height = "100" width = "100"></b>, lalalalalal <p>Testing stuff</p>' +
-		'wooooooo sample text!!!</div>',
-
-		'<div id="content"><div id="siteNotice"></div>'+
-		'<h1 id="firstHeading" class="firstHeading">Front Door</h1>'+
-		'<div id="bodyContent">'+
-		'<p><b><img src = "../testImagePhoebe/fultonHall.jpg" height = "250" width = "300"></b>, lalalalalal <p>Testing stuff</p>' +
-		'wooooooo sample text!!!</div>',
-
-		'<div id="content">'+
-		'<div id="siteNotice">'+
-		'</div>'+
-		'<h1 id="firstHeading" class="firstHeading">Front Door</h1>'+
-		'<div id="bodyContent">'+
-		'<p><b><img src = "../testImagePhoebe/fultonHall.jpg" height = "250" width = "300"></b> lalalalalal <p>Testing stuff</p>' +
-		'wooooooo sample text!!!'+
-		'</div>'
-	];
-
-	//boolean: if a window is up, clicking on the map gets rid of it
-	var windowUp = false;
-	//all pop up windows
-	windows = [];
-
-	//makes sure each marker has different content and different click responses
-	function addInfoWindow(marker,contentString){
-		var flagWindow = new google.maps.InfoWindow({
-			content: contentString
-		});
-		google.maps.event.addListener(marker, 'click', function(){
-			windowUp = true;
-			map: map
-			flagWindow.setPosition(event.latLng);
-			flagWindow.open(map,marker);
-		});
-		windows.push(flagWindow);
-	}
-
-	//calls addInfoWindow for each marker
-	for (i = 0; i< markerArray.length; i++){
-
-		addInfoWindow(markerArray[i],content[i]);
-	}
-
-	console.log('windows: ');
-	console.log(windows);
-
-	//pop up window when click building
-	//var insideBuilding = new google.maps.InfoWindow(); //TODO: deleted this - should I have?
-
-	//if a window (either flag, or a building flag) is open and click map, close the infowindow
-	google.maps.event.addListener(map, 'click', function (event){
-		if (windowUp == true){
-			for (i=0; i<windows.length; i++){
-				windows[i].close();
-			}
-		}
-	});
-
-}*/
 
 /* Converts the list of coordinates stored in the database
  * to an actual usable array
@@ -368,18 +216,6 @@ function addBuilding(building, map) {
 //manages the creation of all buildings on the gui
 //called when the gui is loaded
 function buildGUI() {
-	/*
-	$.ajax({
-		url: '/image',
-		method: 'GET',
-		data: {},
-		success: function(data) {
-			console.log('successfully stored image (?)');
-		}
-	});
-	*/
-
-
 	console.log('in buildGUI');
 	var zoom = 19;
 	var mapCanvas = document.getElementById('map_canvas');
@@ -391,14 +227,13 @@ function buildGUI() {
     };
 	var map = new google.maps.Map(mapCanvas, mapOptions);
 	
+	makeKey(map);
+
 	//populate flags, whenever scrolling changes
 	google.maps.event.addListener(map, 'bounds_changed', function() {
 		addAlerts(map);
 	});
 
-	//addAlerts(map);
-	//addBuilding({latitude: 42.334488, longitude: -71.1701876}, mapCanvas);
-	
 	$.ajax({
 		url: '/buildings',
 		method: 'GET',
@@ -417,271 +252,55 @@ function buildGUI() {
 
 
 
+function makeKey(map) {
+	//MADE A KEY
+	// //need to make dynamic..
+	var key = $('#key');
+	console.log('key: ');
+	console.log(key);
+	map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('key'));
+
+	//consider doing this so screen resizing issues go away???
+	// map.controls[google.maps.ControlPosition.TOP].push(document.getElementById('topBar'));
+
+	key.append('<div>');
+	key.append('<img src= "../testImagePhoebe/caution.png" height = "30" width = "30"> Caution<br />');
+	key.append('<img src= "../testImagePhoebe/wheelchair.jpg" height = "35" width = "35"> Accessible Entrance<br />');
+	key.append('<img src= "../testImagePhoebe/elevatorIcon.png" height = "40" width = "40"> Elevator<br />');
+	key.append('</div>');
+	/*
+	//on double click, creates a marker
+	google.maps.event.addListener(map, 'dblclick', function(event) {
+		var addPin = new google.maps.Marker({
+			map: map,
+			position: event.latLng,
+			draggable: true,
+			animation: google.maps.Animation.DROP,
+			title: "Alert",
+			icon: potholeCaution //change to specific picture? or should pin be added through a form??
+		  });
+
+		//GET RID OF BLUE HIGHLIGHT
+		var contentString = $('<div class = "pin_info">'+ 
+			'<div class = "inner"> Watch out here! </div>'
+			+ '<button class = "remove" title= "Remove"> Remove</button></div>');
 
 
+		var infoWindow = new google.maps.InfoWindow();
+		infoWindow.setContent(contentString[0]);
+		google.maps.event.addListener(addPin, 'click', function(){
+			infoWindow.open(map,addPin);
+		});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//*********** PHOEBE'S FUNCTIONS ************//
-
-// Boston College all-campus coordinates:
-          // center: new google.maps.LatLng(42.3352078, -71.1699536),
-           // zoom: 16,
-//center position: Fulton Hall
-	  
-
-function initialize() { //this is called on load
-	fultonCoords = new google.maps.LatLng(42.334488, -71.1701876);
-        var mapCanvas = document.getElementById('map_canvas');
-        var mapOptions = {
-
-            center: fultonCoords,
-            zoom:19,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-
-//Shape of Fulton Hall Building
-        var fultonShape = new google.maps.Polygon({
-          map: map,
-          paths: [
-            new google.maps.LatLng(42.334553,-71.170432),
-            new google.maps.LatLng(42.334319,-71.170346),
-            new google.maps.LatLng(42.334309,-71.170389),
-            new google.maps.LatLng(42.334247,-71.170368),
-            new google.maps.LatLng(42.334245,-71.170335),
-            new google.maps.LatLng(42.334223,-71.170319),
-            new google.maps.LatLng(42.334220,-71.170290),
-            new google.maps.LatLng(42.334247,-71.170193),
-            new google.maps.LatLng(42.334336,-71.169791),
-            new google.maps.LatLng(42.334323,-71.169751),
-            new google.maps.LatLng(42.334348,-71.169673),
-            new google.maps.LatLng(42.334374,-71.169649),
-            new google.maps.LatLng(42.334444,-71.169670),
-            new google.maps.LatLng(42.334444,-71.169713),
-            new google.maps.LatLng(42.334672,-71.169812),
-            new google.maps.LatLng(42.334668,-71.169845),
-            new google.maps.LatLng(42.334800,-71.169898),
-            new google.maps.LatLng(42.334791,-71.169981),
-            new google.maps.LatLng(42.334749,-71.169971),
-            new google.maps.LatLng(42.334749,-71.170016),
-            new google.maps.LatLng(42.334707,-71.170011),
-            new google.maps.LatLng(42.334656,-71.170263),
-            new google.maps.LatLng(42.334685,-71.170282),
-            new google.maps.LatLng(42.334685,-71.170325),
-            new google.maps.LatLng(42.334713,-71.170346),
-            new google.maps.LatLng(42.334695,-71.170432),
-            new google.maps.LatLng(42.334570,-71.170402)
-          ],
-          strokeColor: '#ff0000',
-          strokeOpacity: 0.8,
-          strokeWeight:2,
-          fillOpacity: 0
-        });
-
-//shape of stokes hall
-        var stokesShape = new google.maps.Polygon({
-          map: map,
-          paths: [
-            new google.maps.LatLng(42.334324,-71.171325),
-            new google.maps.LatLng(42.334348,-71.171325),
-            new google.maps.LatLng(42.334376,-71.171207),
-            new google.maps.LatLng(42.334362,-71.171199),
-            new google.maps.LatLng(42.334362,-71.171164),
-            new google.maps.LatLng(42.334274,-71.171127),
-            new google.maps.LatLng(42.334263,-71.171156),
-            new google.maps.LatLng(42.334005,-71.171065),
-            new google.maps.LatLng(42.334009,-71.171035),
-            new google.maps.LatLng(42.333914,-71.170993),
-            new google.maps.LatLng(42.333902,-71.171044),
-            new google.maps.LatLng(42.333824,-71.171019),
-            new google.maps.LatLng(42.333777,-71.171237),
-            new google.maps.LatLng(42.333757,-71.171242),
-            new google.maps.LatLng(42.333733,-71.171363),
-            new google.maps.LatLng(42.333888,-71.171430),
-            new google.maps.LatLng(42.333864,-71.171542),
-            new google.maps.LatLng(42.334112,-71.171623),
-            new google.maps.LatLng(42.334112,-71.171655),
-            new google.maps.LatLng(42.334191,-71.171674),
-            new google.maps.LatLng(42.334195,-71.171609),
-            new google.maps.LatLng(42.334272,-71.171626)
-
-          ],
-          strokeColor: '#ff0000',
-          strokeOpacity: 0.8,
-          strokeWeight:2,
-          fillOpacity: 0
-        });
-
-//event listener when click stokes shape
-//WILL CLEAN THIS UP LATER
-google.maps.event.addListener(stokesShape, 'click', showStokesModal);
-//color changes when mousing over the building
-google.maps.event.addListener(stokesShape, 'mouseover', function (event){
-  this.setOptions({
-    strokeColor: '#ff0000',
-    fillColor: '#ff0000',
-    fillOpacity: 0.5
-  });
-});
-
-//removes color when mouse out of the building
-google.maps.event.addListener(stokesShape, 'mouseout', function (event){
-  this.setOptions({
-    strokeColor: '#ff0000',
-    fillOpacity: 0
-  });
-  
-});
-
-//end stokes stuff
-
-//event listener when click shape
-google.maps.event.addListener(fultonShape, 'click', showFultonModal);
-
-//pop up window when click building
-insideBuilding = new google.maps.InfoWindow();
-
-//boolean: if a window is up, clicking on the map gets rid of it
-var windowUp = false;
-
-// //loads contents of popup window; opens up the popup window 
-// function showFloors(event){
-//   windowUp = true;
-//   map: map
-//   var contentString = '<div id="content">'+
-//       '<div id="siteNotice">'+
-//       '</div>'+
-//       '<h1 id="firstHeading" class="firstHeading">First Floor</h1>'+
-//       '<div id="bodyContent">'+
-//       '<p><b>Insert Floorplan here</b>, lalalalalal <p>Testing stuff</p>' +
-//       'wooooooo sample text!!!'+
-//       '</div> <img src = "../testImagePhoebe/phoebePicTest.jpg" height = "500" width = "500">'
-//       +'</div>';
-//   insideBuilding.setContent(contentString);
-//   insideBuilding.setPosition(fultonCoords);
-//   insideBuilding.open(map);
-//   windows.push(insideBuilding);
-// }
-
-//color changes when mousing over the building
-google.maps.event.addListener(fultonShape, 'mouseover', function (event){
-  this.setOptions({
-    strokeColor: '#ff0000',
-    fillColor: '#ff0000',
-    fillOpacity: 0.5
-  });
-});
-
-//removes color when mouse out of the building
-google.maps.event.addListener(fultonShape, 'mouseout', function (event){
-  this.setOptions({
-    strokeColor: '#ff0000',
-    fillOpacity: 0
-  });
-  
-});
-
-//all pop up windows
-windows = [];
-//if a window (either flag, or a building flag) is open and click map, close the infowindow
-  google.maps.event.addListener(map, 'click', function (event){
-      if (windowUp == true){
-        for (i=0; i<windows.length; i++){
-          windows[i].close();
-        }
-      }
-  });
-
-potholeLocations = [new google.maps.LatLng(42.334379,-71.169555)];
-doorLocations = [new google.maps.LatLng(42.334294,-71.169987), new google.maps.LatLng(42.334635,-71.169783)];
-
-//instantiating door symbol
-var wheelchairDoor = {
-  url: '../testImagePhoebe/wheelchair.jpg',
-  scaledSize: new google.maps.Size(25,25)
-};
-//instantiating pothole caution symbol
-var potholeCaution = {
-  url: '../testImagePhoebe/caution.png',
-  scaledSize:new google.maps.Size(30,25)
+		var removePin =contentString.find('button.remove')[0];
+		google.maps.event.addDomListener(removePin, "click", function(event){
+			addPin.setMap(null);
+		});
+	});
+	*/
 }
 
-//adding markers
-markerArray = [];
-function addMarkers(array, iconType){
-  for (i = 0; i < array.length; i++){
-    var marker = new google.maps.Marker({
-      position: array[i],
-      icon: iconType,
-      map: map,
-      draggable: false
-    })
-    markerArray.push(marker);
-  }
-}
-
-//index 0
-addMarkers(potholeLocations, potholeCaution);
-//indices 1-2
-addMarkers(doorLocations, wheelchairDoor);
 
 
-//html content for each  marker popup
-content = ['<div id="content"> <div id="siteNotice"></div>'+
-      '<h1 id="firstHeading" class="firstHeading">Pothole</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b><img src = "../testImagePhoebe/pothole.jpg" height = "100" width = "100"></b>, lalalalalal <p>Testing stuff</p>' +
-      'wooooooo sample text!!!</div>',
-
-      '<div id="content"><div id="siteNotice"></div>'+
-      '<h1 id="firstHeading" class="firstHeading">Front Door</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b><img src = "../testImagePhoebe/fultonHall.jpg" height = "250" width = "300"></b>, lalalalalal <p>Testing stuff</p>' +
-      'wooooooo sample text!!!</div>',
-
-      '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Front Door</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b><img src = "../testImagePhoebe/fultonHall.jpg" height = "250" width = "300"></b> lalalalalal <p>Testing stuff</p>' +
-      'wooooooo sample text!!!'+
-      '</div>'
-      ];
-
-
-//makes sure each marker has different content and different click responses
-function addInfoWindow(marker,contentString){
-  var flagWindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-  google.maps.event.addListener(marker, 'click', function(){
-    windowUp = true;
-    map: map
-    flagWindow.setPosition(event.latLng);
-    flagWindow.open(map,marker);
-  });
-  windows.push(flagWindow);
-}
-
-//calls addInfoWindow for each marker
-for (i = 0; i< markerArray.length; i++){
-  addInfoWindow(markerArray[i],content[i]);
-}
-
-}
 //google.maps.event.addDomListener(window, 'load', initialize);
 google.maps.event.addDomListener(window, 'load', buildGUI);
